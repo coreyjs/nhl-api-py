6b8209a13d795d85b6220f575c5e7b26055fc565
Corey Schaf <cschaf@gmail.com>
2023-10-03 10:15:56 -0400

    updates get_schedule to use new date param (#9)

--- tests/test_schedule.py
+++ tests/test_schedule.py
@@ -19,3 +19,8 @@ def test_get_schedule_with_season_and_game_type(h_m, nhl_client):
 def test_get_schedule_with_game_type(h_m, nhl_client):
     nhl_client.schedule.get_schedule(game_type="PR")
     h_m.assert_called_once_with(url="https://statsapi.web.nhl.com/api/v1/schedule?gameType=PR")
+
+@mock.patch("httpx.get")
+def test_get_schedule_with_date(h_m, nhl_client):
+    nhl_client.schedule.get_schedule(date="2022-10-01")
+    h_m.assert_called_once_with(url="https://statsapi.web.nhl.com/api/v1/schedule?date=2022-10-01")
