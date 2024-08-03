abd0337f9670a73c7e573a2d323ceb46fa170809
Corey Schaf <cschaf@gmail.com>
2024-08-03 10:10:14 -0400

    59 utah (#60)
    * Update/Append:  Adds Utah Hockey Club to franchise info and hard file information for team info and ids.
    
    Deprecates:  Teams.team_stats
    
    * version bump

--- nhlpy/api/teams.py
+++ nhlpy/api/teams.py
@@ -1,6 +1,5 @@
 import json
 import importlib.resources
-from warnings import warn
 from typing import List
 
 from nhlpy.http_client import HttpClient
@@ -12,29 +11,11 @@ class Teams:
         self.base_url = "https://api.nhle.com"
         self.api_ver = "/stats/rest/"
 
-    # todo deprecate this
-    def team_stats_summary(self, lang="en") -> List[dict]:
-        """
-        I really don't know what this endpoint does.  It returns a list of dicts with team stats.
-        :param lang: Language param.  'en' for English, 'fr' for French
-        :return:
-        """
-        warn(
-            "This endpoint will be deprecated in the future.  Use stats.team_summary() instead.",
-            DeprecationWarning,
-            stacklevel=2,
-        )
-        return self.client.get_by_url(full_resource=f"{self.base_url}{self.api_ver}{lang}/team/summary").json()["data"]
-
     def teams_info(self) -> dict:
         """
         Returns a list of dicts with all the teams info.  This is loaded via a harccoded json file.
         :return: dict
         """
-
-        # with importlib.resources.files("nhlpy.data", "teams_20232024.json") as f:
-        #     return json.load(f)['teams']
-
         data_resource = importlib.resources.files("nhlpy") / "data"
         teams_info = json.loads((data_resource / "teams_20232024.json").read_text())["teams"]
         return teams_info
