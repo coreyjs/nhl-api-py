503a7e892e594a46d23a410f44a85da46b02ba3a
Corey Schaf <cschaf@gmail.com>
2024-10-08 11:35:42 -0400

    Schedule API Adjustments (#71)
    * Updates:
    Schedule API: schedule.py
    - get_schedule(date) no longer has an optional parameter 'date'.  This is now a required parameter.  This method now will reduce down the response payload to only the games for the supplied date.  This is a change from the old way.  Old functionality has been moved to 'get_weekly_schedule'
    - get_weekly_schedule(date?) has been added to return the weekly games, for the week of the supplied date.  Data is still optional but defaults to the NHL interrepation of 'now' or 'today'.
    
    - Adds new tests to cover functionality in these updated schedule endpoints
    
    * Updates: README.md

--- nhlpy/api/schedule.py
+++ nhlpy/api/schedule.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from typing import Optional, List
 
 from nhlpy.http_client import HttpClient
@@ -7,10 +8,39 @@ class Schedule:
     def __init__(self, http_client: HttpClient) -> None:
         self.client = http_client
 
-    def get_schedule(self, date: Optional[str] = None) -> dict:
+    def get_schedule(self, date: str = None) -> dict:
         """
-        Get the schedule for the NHL for the given date.  If no date is supplied it will
-        default to today.
+        Get the schedule for the NHL for the given date.  Contains only games for the given date.
+        :param date:  In format YYYY-MM-DD.
+        :return: dict
+        """
+        try:
+            # Parse and reformat the date to ensure YYYY-MM-DD
+            date = datetime.strptime(date, "%Y-%m-%d").strftime("%Y-%m-%d")
+        except ValueError:
+            raise ValueError("Invalid date format. Please use YYYY-MM-DD.")
+
+        schedule_data: dict = self.client.get(resource=f"schedule/{date}").json()
+        response_payload = {
+            "nextStartDate": schedule_data["nextStartDate"],
+            "previousStartDate": schedule_data["previousStartDate"],
+            "date": date,
+        }
+
+        matching_day = next((day for day in schedule_data["gameWeek"] if day["date"] == date), None)
+
+        if matching_day:
+            response_payload["games"] = matching_day["games"]
+            response_payload["numberOfGames"] = len(matching_day["games"])
+
+        return response_payload
+
+    def get_weekly_schedule(self, date: Optional[str] = None) -> dict:
+        """
+        Get the schedule for the NHL for the week of `date`.  If no date is supplied it will
+        default to today. What the "NHL" deems as "today" seems to switch over around 12 est.  Its preferred you
+        supply a date
+
         :param date:  In format YYYY-MM-DD.  If no date is supplied, it will default to "Today".  Which in case
             of the NHL could be today or yesterday depending on how early you call it.
         :return: dict
