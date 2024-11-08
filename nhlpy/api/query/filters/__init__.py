1993bffde4c51811d6feafa9cfe7018389f8483d
Corey Schaf <cschaf@gmail.com>
2024-11-08 15:07:44 -0500

    Fix: ShootCatchQuery - Filter now properly serializes L or R values. (#78)
    * Fix: ShootCatchQuery - Filter now properly serializes L or R values.
    - Fixes test for this change.
    - Refactors goalie default sorts into init.
    
    * patch increase

--- nhlpy/api/query/filters/__init__.py
+++ nhlpy/api/query/filters/__init__.py
@@ -1,5 +1,5 @@
 from abc import ABC, abstractmethod
-from typing import Union
+from typing import Union, List
 
 
 class QueryBase(ABC):
@@ -10,3 +10,61 @@ class QueryBase(ABC):
     @abstractmethod
     def validate(self) -> Union[bool, None]:
         return True
+
+
+def _goalie_stats_sorts(report: str) -> List[dict]:
+    """
+    This is default criteria for sorting on goalie stats.  I hate this method
+    :param report:
+    :return:
+    """
+    if report == "summary":
+        return [
+            {"property": "wins", "direction": "DESC"},
+            {"property": "gamesPlayed", "direction": "ASC"},
+            {"property": "playerId", "direction": "ASC"},
+        ]
+    elif report == "advanced":
+        return [
+            {"property": "qualityStart", "direction": "DESC"},
+            {"property": "goalsAgainstAverage", "direction": "ASC"},
+            {"property": "playerId", "direction": "ASC"},
+        ]
+    elif report == "bios":
+        return [
+            {"property": "lastName", "direction": "ASC_CI"},
+            {"property": "goalieFullName", "direction": "ASC_CI"},
+            {"property": "playerId", "direction": "ASC"},
+        ]
+    elif report == "daysrest":
+        return [
+            {"property": "wins", "direction": "DESC"},
+            {"property": "savePct", "direction": "DESC"},
+            {"property": "playerId", "direction": "ASC"},
+        ]
+    elif report == "penaltyShots":
+        return [
+            {"property": "penaltyShotsSaves", "direction": "DESC"},
+            {"property": "penaltyShotSavePct", "direction": "DESC"},
+            {"property": "playerId", "direction": "ASC"},
+        ]
+    elif report == "savesByStrength":
+        return [
+            {"property": "wins", "direction": "DESC"},
+            {"property": "savePct", "direction": "DESC"},
+            {"property": "playerId", "direction": "ASC"},
+        ]
+    elif report == "shootout":
+        return [
+            {"property": "shootoutWins", "direction": "DESC"},
+            {"property": "shootoutSavePct", "direction": "DESC"},
+            {"property": "playerId", "direction": "ASC"},
+        ]
+    elif report == "startedVsRelieved":
+        return [
+            {"property": "gamesStarted", "direction": "DESC"},
+            {"property": "gamesStartedSavePct", "direction": "DESC"},
+            {"property": "playerId", "direction": "ASC"},
+        ]
+    else:
+        return [{}]
--- tests/query/filters/test_shoot_catch.py
+++ tests/query/filters/test_shoot_catch.py
@@ -3,9 +3,9 @@ from nhlpy.api.query.filters.shoot_catch import ShootCatchesQuery
 
 def test_shoot_catch_l():
     shoot_catch = ShootCatchesQuery(shoot_catch="L")
-    assert shoot_catch.to_query() == "shootsCatches=L"
+    assert shoot_catch.to_query() == "shootsCatches='L'"
 
 
 def test_shoot_catch_r():
     shoot_catch = ShootCatchesQuery(shoot_catch="R")
-    assert shoot_catch.to_query() == "shootsCatches=R"
+    assert shoot_catch.to_query() == "shootsCatches='R'"
