1b8798a896f04df72426440b436f969fbb5e4df0
Corey Schaf <cschaf@gmail.com>
2024-12-09 09:11:27 -0500

    Feature: Helpers module. - Contains useful but often expiremental methods for aggregation and analysis

--- nhlpy/api/helpers.py
+++ nhlpy/api/helpers.py
@@ -7,10 +7,11 @@ class Helpers:
     def __init__(self, http_client: HttpClient) -> None:
         self.client = http_client
 
-    def get_gameids_by_season(self, season: str) -> List[str]:
+    def get_gameids_by_season(self, season: str, game_types: List[int] = None) -> List[str]:
         """
         Get all gameids for a given season.
         :param season: The season you want the gameids for.  Format is YYYYYYYY.  20202021, 200232024, etc
+        :param game_types: List of game types you want to include.  2 is regular season, 3 is playoffs, 1 is preseason
         """
         from nhlpy.api.teams import Teams
         from nhlpy.api.schedule import Schedule
@@ -20,6 +21,8 @@ class Helpers:
         gameids = []
         for team in teams:
             schedule = Schedule(self.client).get_season_schedule(team["abbr"], season)
-            gameids.extend([game["id"] for game in schedule["games"]])
+            for game in schedule["games"]:
+                if not game_types or game["gameType"] in game_types:
+                    gameids.append(game["id"])
 
         return gameids
